a006f25edb5a8ef6dc3b92518c69c936d0d74517
Carla <cm.stacchiola@alumno.um.edu.ar>
2024-04-03 14:24:27 -0300

    numeros primos

--- n.primos.py
+++ n.primos.py
@@ -7,17 +7,10 @@ def is_primo(value):
     # valor % div -> resto
 
     if value % 2 != 0:
-
         return True
 
     else:
-
         return False
-
-    
-
-
-
 
 
 class TestPrimos(unittest.TestCase):
